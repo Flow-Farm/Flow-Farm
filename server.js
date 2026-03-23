efae3b8d5b5f5dfc4b353c05be6380263f5a4adf
Michaela Samu <150352176+Mimsum7@users.noreply.github.com>
2026-03-23 16:55:04 +0800

    Added test route to test front end cloud connection

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_URI = process.env.MONGODB_URI;
 
+// This route provides the data Blessed's PerformanceChart.jsx is looking for
+app.get('/api/sensors', (req, res) => {
+  res.json({
+    labels: ["9am", "10am", "11am", "12pm", "1pm"],
+    tempData: [28, 29, 30, 31, 32],
+    waterData: [22, 40, 55, 61, 32],
+    phData: [6, 7, 7.5, 8, 8.1]
+  });
+});
+
 // Attempt to connect to the database first
 mongoose.connect(DB_URI)
   .then(() => {
